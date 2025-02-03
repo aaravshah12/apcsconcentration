3cdb75d75a9362ce75a8b7df29d096afb2e7a088
Aarav Shah <codingaarav1@gmail.com>
2025-02-03 14:36:02 -0800

    sigmaboy

---
+++ Board.java
@@ -0,0 +1,197 @@
+/** 
+ * A game board of NxM board of tiles.
+ * 
+ *  @author PLTW
+ * @version 2.0
+ */
+
+/** 
+ * A Board class for concentration
+ */
+import java.util.Arrays;
+import java.util.List;
+import java.util.Collections;
+public class Board
+{  
+  private static String[] tileValues = {"lion", "lion",
+                                        "penguin", "penguin",
+                                        "dolphin", "dolphin",
+                                        "fox", "fox",
+                                        "monkey", "monkey",
+                                        "turtle", "turtle",
+                                        "Aster", "Aster",
+                                        "Aarav", "Aarav",
+                                        "wolf", "wolf",
+                                        "tiger", "tiger",
+                                        "donkey", "donkey",
+                                        "rhino", "rhino",
+                                        "chicken", "chicken",
+                                        "dog", "dog",
+                                        "cow", "cow"                                      
+                                      }; 
+  private static final int rows = 6;
+  private static final int cols = 5;
+  public static int getRows() {
+    return rows;
+  }
+
+  public static int getCols() {
+    return cols;
+  }
+
+  private Tile[][] gameboard = new Tile[rows][cols];
+
+  public Tile[][] getGameboard() {
+    return gameboard;
+  }
+
+  /**  
+   * Constructor for the game. Creates the 2D gameboard
+   * by populating it with card values
+   * 
+   */
+  public Board()
+  {
+    int counter = 0;
+    List<String> cool = Arrays.asList(tileValues);
+    Collections.shuffle(cool);
+    cool.toArray(tileValues);
+    /* your code here */ 
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        gameboard[i][j] = new Tile(tileValues[counter]);
+        counter++;
+      }
+    }
+
+  }
+
+ /** 
+   * Returns a string representation of the board, getting the state of
+   * each tile. If the tile is showing, displays its value, 
+   * otherwise displays it as hidden.
+   * 
+   * Precondition: gameboard is populated with tiles
+   * 
+   * @return a string represetation of the board
+   */
+  public String toString()
+  {
+ 
+    /* your code here */
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        if (gameboard[i][j].isShowingValue()) {
+          System.out.print(gameboard[i][j].getValue());
+        }
+        else {
+          System.out.print(gameboard[i][j].getHidden());
+        }
+        System.out.print(" ");
+      }
+      System.out.println();
+    }
+    return "";
+  }
+
+  /** 
+   * Determines if the board is full of tiles that have all been matched,
+   * indicating the game is over.
+   * 
+   * Precondition: gameboard is populated with tiles
+   * 
+   * @return true if all tiles have been matched, false otherwse
+   */
+  public boolean allTilesMatch()
+  {
+
+    /* your code  here */
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        if(!gameboard[i][j].matched()) return false;
+      }
+    }
+    
+    return true;
+  }
+
+  /** 
+   * Sets the tile to show its value (like a playing card face up)
+   * 
+   * Preconditions:
+   *   gameboard is populated with tiles,
+   *   row values must be in the range of 0 to gameboard.length,
+   *   column values must be in the range of 0 to gameboard[0].length
+   * 
+   * @param row the row value of Tile
+   * @param column the column value of Tile
+   */
+  public void showValue (int row, int column)
+  {
+   
+    /* your code here */
+    Tile t = gameboard[row][column];
+    t.show();
+  }  
+
+  /** 
+   * Checks if the Tiles in the two locations match.
+   * 
+   * If Tiles match, show Tiles in matched state and return a "matched" message
+   * If Tiles do not match, re-hide Tiles (turn face down).
+   * 
+   * Preconditions:
+   *   gameboard is populated with Tiles,
+   *   row values must be in the range of 0 to gameboard.length,
+   *   column values must be in the range of 0 to gameboard[0].length
+   * 
+   * @param row1 the row value of Tile 1
+   * @param col1 the column value of Tile 1
+   * @param row2 the row value of Tile 2
+   * @param col2 the column vlue of Tile 2
+   * @return a message indicating whether or not a match occured
+   */
+  public String checkForMatch(int row1, int col1, int row2, int col2)
+  {
+    String msg = "";
+
+     /* your code here */
+     Tile tile1 = gameboard[row1][col1];
+     Tile tile2 = gameboard[row2][col2];
+     if (tile1.equals(tile2)) {
+      tile1.foundMatch();
+      tile2.foundMatch();
+      msg = "Matched!";
+     } else {
+      tile1.hide();
+      tile2.hide();
+     }
+    
+     return msg;
+  }
+
+  /** 
+   * Checks the provided values fall within the range of the gameboard's dimension
+   * and that the tile has not been previously matched
+   * 
+   * @param rpw the row value of Tile
+   * @param col the column value of Tile
+   * @return true if row and col fall on the board and the row,col tile is unmatched, false otherwise
+   */
+  public boolean validateSelection(int row, int col)
+  {
+
+    /* your code here */
+    if (row > rows - 1 || row < 0) {
+      return false;
+    }
+    if (col > cols - 1 || col < 0) {
+      return false;
+    }
+    if(gameboard[row][col].matched()) {
+      return false;
+    }
+    return true;
+  }
+
+}
---
+++ GameGUI.java
@@ -0,0 +1,183 @@
+/** 
+ * The game that uses a n x m board of tiles or cards.
+ *  
+ * Player chooses two random tiles from the board. The chosen tiles
+ * are temporarily shown face up. If the tiles match, they are removed from board.
+ * 
+ * Play continues, matching two tiles at a time, until all tles have been matched.
+ * 
+ * @author PLTW
+ * @version 2.0
+*/
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * A game class to play concentration
+ */
+public class GameGUI
+{
+  private Board board;
+  JFrame frame;
+  JPanel buttonPanel;
+  private int row1, col1;
+  private int row2, col2;
+  private int row, col;
+  int turnNumber = 0;
+  int scoreP1 = 0;
+  int scoreP2 = 0;
+  public void play() {
+    board = new Board();
+
+    frame = new JFrame("2D Button Grid");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(1000, 1000);
+    frame.setLayout(new BorderLayout()); 
+
+    buttonPanel = new JPanel();
+    buttonPanel.setLayout(new GridLayout(Board.getRows(), Board.getCols(), 10, 10)); // Add gaps for spacing
+
+    frame.add(buttonPanel, BorderLayout.CENTER); // Make buttonPanel fill the frame
+
+    frame.setVisible(true);
+    showBoard();
+    while (!board.allTilesMatch())
+    {
+      row1 = -1;
+      col1 = -1;
+      boolean validTile = false;
+      while (!validTile)
+      {
+        if(row != -1) {
+          validTile = board.validateSelection(row1, col1);
+
+          board.showValue(row, col);
+          row1 = row;
+          col1 = col;
+          row = -1;
+          col = -1;
+        }
+      }
+
+      // display first tile
+      board.showValue(row1, col1);
+      showBoard();
+
+      // get player's second selection, if not an integer, quit
+      row2 = -1;
+      col2 = -1;
+      validTile = false;
+      while (!validTile)
+      {
+        System.out.print("");
+        if(row != -1) {
+          row2 = row;
+          col2 = col;
+          row = -1;
+          col = -1;
+          validTile = board.validateSelection(row2, col2);
+        }
+      }
+      
+
+      // display second tile
+      board.showValue(row2, col2);
+      showBoard();
+
+      String matched = board.checkForMatch(row1, col1, row2, col2);
+      System.out.println(matched);
+      if (matched.equals("Matched!")) {
+        if (turnNumber % 2 == 0) scoreP1 ++;
+        else scoreP2 ++;
+      }
+      turnNumber++;
+
+      wait(2); 
+      
+    }
+
+    showBoard();
+    System.out.println("Game Over!");
+  }
+
+  private boolean getTile(boolean firstChoice)
+  {
+    int num1 = 0;
+    int num2 = 0;
+
+
+    if (!board.validateSelection(num1, num2))
+    {
+      System.out.print("Invalid input, please try again. ");
+      wait(2);
+      return false;
+    }
+    if (firstChoice)   
+    {
+      row1 = num1;
+      col1 = num2;
+    }
+    else 
+    {
+      row2 = num1;
+      col2 = num2;
+    }
+    return true;
+  }
+
+  /**
+   * Clear the console and show the game board
+   */
+  public void showBoard() {
+    buttonPanel.removeAll();
+    Tile[][] gameboard = board.getGameboard();
+    
+    ActionListener buttonListener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JButton sourceButton = (JButton) e.getSource();
+              row = (int)sourceButton.getClientProperty("row");
+              col = (int)sourceButton.getClientProperty("col");
+          }
+    };
+
+    for (int i = 0; i < Board.getRows(); i++) {
+      buttonPanel.setLayout(new GridLayout(Board.getRows(), Board.getCols()));
+        for (int j = 0; j < Board.getCols(); j++) {
+            String value;
+            Tile tile = gameboard[i][j];
+            if (gameboard[i][j].isShowingValue()) {
+                value = tile.getValue();
+              }
+              else {
+                value = tile.getHidden();
+              }
+            JButton button = new JButton(value);
+            button.setMinimumSize(new Dimension(300, 300));
+            button.addActionListener(buttonListener);
+            button.putClientProperty("tile", tile);
+            button.putClientProperty("row", i);
+            button.putClientProperty("col", j);
+            buttonPanel.add(button);
+        }
+    }
+    buttonPanel.validate();
+
+  }
+
+  private void wait(int n)
+  {
+    try
+    {
+      Thread.sleep(n * 1000);
+    } catch (InterruptedException e) { /* do nothing */ }
+  }
+
+  private void quitGame() 
+  {
+    System.out.println("Quit game!");
+    System.exit(0);
+  }
+}
